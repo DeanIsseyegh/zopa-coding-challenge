ca1c3c1d3e263772448827a88ec783adbac3bf31
DeanIsseyegh <dean.isseyegh@secretescapes.com>
2017-08-20 20:13:31 +0100

    Added main app to take in required arguments

--- src/main/java/dean/zopa/Main.java
+++ src/main/java/dean/zopa/Main.java
@@ -1,5 +1,7 @@
 package dean.zopa;
 
+import dean.zopa.csv.InputParser;
+import dean.zopa.csv.InputValidator;
 import dean.zopa.lender.Lender;
 import dean.zopa.lender.LenderPool;
 import dean.zopa.logic.LoanAlgorithm;
@@ -15,55 +17,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
 	public static void main(String... args) throws IOException {
-		Lender lender1 = new Lender("Bob", new BigDecimal("0.075"), Money.of(640, Config.CURRENCY));
-		Lender lender2 = new Lender("Jane", new BigDecimal("0.069"), Money.of(480, Config.CURRENCY));
-		Lender lender3 = new Lender("Fred", new BigDecimal("0.071"), Money.of(520, Config.CURRENCY));
-		Lender lender4 = new Lender("Mary", new BigDecimal("0.104"), Money.of(170, Config.CURRENCY));
-		Lender lender5 = new Lender("John", new BigDecimal("0.081"), Money.of(320, Config.CURRENCY));
-		Lender lender6 = new Lender("Dave", new BigDecimal("0.074"), Money.of(140, Config.CURRENCY));
-		Lender lender7 = new Lender("Angela", new BigDecimal("0.071"), Money.of(60, Config.CURRENCY));
-
-		LenderPool lenderPool = new LenderPool(Arrays.asList(lender1, lender2, lender3, lender4, lender5, lender6, lender7));
-
+		String filePath = args[0];
+		String amountRequested = args[1];
+		Stream<String> stream = Files.lines(Paths.get(filePath));
+		List<String> fileLines = stream.collect(Collectors.toList());
+		InputParser inputParser = new InputParser(fileLines, new InputValidator());
+		List<Lender> lenders = inputParser.parseLenders();
+		LenderPool lenderPool = new LenderPool(lenders);
 		LoanCalculator loanCalculator = new LoanCalculator(new LoanAlgorithm(lenderPool));
-
-		Map<Lender, MonetaryAmount> amountsToBorrowPerLender = loanCalculator.calcAmountToBorrowPerLender(Money.of(200, Config.CURRENCY));
-		MonetaryAmount totalAmount = Money.of(0, Config.CURRENCY);
-		ArrayList<BigDecimal> rates = new ArrayList<>();
-			for (Map.Entry<Lender, MonetaryAmount> mapEntry: amountsToBorrowPerLender.entrySet()) {
-				totalAmount = totalAmount.add(mapEntry.getValue());
-				rates.add(mapEntry.getKey().getRate());
-			}
-
-		BigDecimal totalRate = rates.stream().reduce(BigDecimal::add).get();
-		BigDecimal averageWeight = totalRate.divide(new BigDecimal(rates.size()), 6, BigDecimal.ROUND_HALF_UP);
-		averageWeight = averageWeight.multiply(new BigDecimal("100")).setScale(2, BigDecimal.ROUND_HALF_UP);
-		System.out.println("Amount to lend: " + totalAmount);
-		System.out.println("Average weight: " + averageWeight);
-
-
-		String content = new String(Files.readAllBytes(Paths.get("market.csv")));
-
-	/*	CsvMapper mapper = new CsvMapper();
-		CsvSchema schema = mapper.schemaFor(User.class);
-		schema = schema.withColumnSeparator('\t').withHeader();
-
-		System.out.println(content);
-		String csv = mapper.writer(schema).writeValueAsString(content);
-		MappingIterator<User> it = mapper.readerFor(User.class).with(schema).readValues(csv);
-		while (it.hasNextValue()) {
-			User value = it.nextValue();
-			System.out.println(value.getName());
-		}*/
-
-		File tempFile = new File("market.csv");
-
-
+		Quote quote = new Quote(Money.of(new BigDecimal(amountRequested), Config.CURRENCY), loanCalculator);
+		System.out.println(quote.toString());
 	}
 
-	//TODO: Deal with rounding issues/edge cases
 }
